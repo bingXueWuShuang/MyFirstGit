122f0c42be380c50be1f1f279979abb82db22686
bingXueWuShuang <2744806951@qq.com>
2017-01-06 18:14:35 +0800

    提交到本地库

--- FirstGit/app/src/main/java/qf/com/firstgit/MainActivity.java
+++ FirstGit/app/src/main/java/qf/com/firstgit/MainActivity.java
@@ -2,6 +2,7 @@ package qf.com.firstgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "哈哈哈哈", Toast.LENGTH_SHORT).show();
     }
 }
